8a1f748901297d08569e0edc277f0a9719143cd4
Aman Singh <102784256+mov-aman@users.noreply.github.com>
2023-07-13 06:01:07 +0530

    Add files via upload

---
+++ Loops.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+/*        for(int i = 0; i <= 10; i++)
+        {
+            Debug.Log("Value of i is : " + i);
+        }*/
+       int i = 0;
+ /*       while (i <= 10) ;
+        {
+            Debug.Log("Value of i is : " + i);
+            i++;
+        }*/
+        do
+        {
+            Debug.Log("Value of i is : " + i);
+            i++;
+        }
+        while (i <= 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
