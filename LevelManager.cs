6d7b7010cd1ae761d65ec6df3445b137f08f4b87
Aman Singh <102784256+mov-aman@users.noreply.github.com>
2023-07-18 03:30:04 +0530

    Add files via upload

---
+++ LevelManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public Transform instantiatePos;
+    void Start()
+    {
+        Instantiate(ballPrefab, instantiatePos.position, Quaternion.identity);
+    }
+}
