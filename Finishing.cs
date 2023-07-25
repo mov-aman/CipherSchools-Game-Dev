b36590dc7864264b6d1e426f7270623ec9e98cdd
Aman Singh <102784256+mov-aman@users.noreply.github.com>
2023-07-25 17:25:00 +0530

    Create Finishing.cs

---
+++ Finishing.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroUIHandler : MonoBehaviour
+{
+    public GameObject instructionPanel;
+
+    public void ToggleInstructionPanel(string toggle)
+    {
+        if(toggle == "false")
+        {
+            instructionPanel.SetActive(false);
+        }
+        else if(toggle == "true")
+        {
+            instructionPanel.SetActive(true);
+        }
+    }
+    public void ChangeCurrentScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        int nextIndex = currentScene.buildIndex + 1;
+
+        SceneManager.LoadScene(nextIndex);
+    }
+}
